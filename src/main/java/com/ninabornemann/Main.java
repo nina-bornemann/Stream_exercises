8fce308e9c73f6d62c2e61da95608b6504c7205c
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-06 15:43:05 -0700

    add exercise 4 and 5

--- src/main/java/com/ninabornemann/Main.java
+++ src/main/java/com/ninabornemann/Main.java
@@ -3,6 +3,7 @@ package com.ninabornemann;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -12,12 +13,29 @@ public class Main {
 
         // get all odd numbers, double, sort,
         List<Integer> filteredNums = numberList.stream()
-            .filter(i -> i % 2 != 0)
+                .filter(Main::isEven)
                 .map(i -> i * 2)
                 .sorted()
                 .collect(Collectors.toList());
         System.out.println(filteredNums);
 
+        // get sum of all
+        int sumNum =numberList.stream()
+                .reduce(0, Integer::sum);
+        System.out.println(sumNum);
 
-        }
+        // use forEach, ends the stream
+        numberList.forEach(System.out::println);
+
+        // collect
+        List<Boolean> collected = numberList.stream()
+                .map(i -> i % 2 == 0)
+                .toList();
     }
+
+    public static boolean isEven(Integer i) {
+        return i % 2 == 0;
+    }
+}
+
+
