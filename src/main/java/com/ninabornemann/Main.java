4be891062189d6a8afd9da8000e5602d7f03ef82
Nina Bornemann <nina.bornemann@freenet.de>
2025-09-06 03:31:29 -0700

    add tasks 1-3

--- src/main/java/com/ninabornemann/Main.java
+++ src/main/java/com/ninabornemann/Main.java
@@ -1,8 +1,23 @@
 package com.ninabornemann;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class Main {
     public static void main(String[] args) {
 
+        List<Integer> numberList = Arrays.asList(5, 4, 7, -1, 12);
 
+        // get all odd numbers, double, sort,
+        List<Integer> filteredNums = numberList.stream()
+            .filter(i -> i % 2 != 0)
+                .map(i -> i * 2)
+                .sorted()
+                .collect(Collectors.toList());
+        System.out.println(filteredNums);
+
+
+        }
     }
-}
